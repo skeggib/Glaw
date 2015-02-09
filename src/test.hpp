3d1e096683705da4ae5fa61639b15e7a2d45cdb8
Sebastien Klasa <klasa.sebastien@gmail.com>
2015-02-09 22:25:15 +0100

    Moved main to test()
    	modified:   src/main.cpp
    	renamed:    src/main.cpp -> src/test.cpp
    	new file:   src/test.hpp

---
+++ src/test.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <math.h>
+
+#include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+using namespace std;
+
+#include "events/events.hpp"
+#include "operator-overloading/op_coord.hpp"
+#include "triangle.hpp"
+#include "color.hpp"
+#include "coord.hpp"
+#include "vector.hpp"
+#include "object.hpp"
+
+int test();
