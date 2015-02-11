5f55d8c14269ec823ee4fc0707efe02d55d74a60
Sebastien Klasa <klasa.sebastien@gmail.com>
2015-02-11 01:21:30 +0100

    alpha v0.1.0

---
+++ glaw.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "src/events/events.hpp"
+#include "src/vector.hpp"
+#include "src/color.hpp"
+#include "src/coord.hpp"
+#include "src/triangle.hpp"
+#include "src/object.hpp"
