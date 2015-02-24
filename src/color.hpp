c427e9205f59a5434c5252b18376f6468d1637db
Sebastien Klasa <klasa.sebastien@gmail.com>
2015-02-24 21:47:14 +0100

    Overloaded Color constructor

--- src/color.hpp
+++ src/color.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+/*! \fn Color
+ *	
+ *	RGBA color
+ *	
+*/
 struct Color
 {
     int r;
@@ -9,4 +14,5 @@ struct Color
 
     Color() : r(0), g(0), b(0), a(0) {};
     Color(int r, int g, int b, int a) : r(r), g(g), b(b), a(a) {};
+    Color(int r, int g, int b) : r(r), g(g), b(b), a(255) {};
 };
